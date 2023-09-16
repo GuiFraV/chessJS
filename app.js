0004213f6b1ad4eaef12ef60c2c6d4a83ffde202
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-16 10:41:31 +0200

    add chess pieces

---
+++ app.js
@@ -0,0 +1,8 @@
+const gameboard = document.querySelector('#gameboard')
+const plauerDisplay = document.querySelector('#player')
+const inforDisplay = document.querySelector('#info-display')
+const width = 8
+
+const startPieces = [
+    
+]
