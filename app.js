e8db9f596d15d02c905c89d0d854b4151c57f339
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-16 10:57:49 +0200

    chess pieces TADAAA

--- app.js
+++ app.js
@@ -16,8 +16,14 @@ const startPieces = [
 ]
 
 function createBoard(){
-    startPieces.forEach((startPiece) => {
+    startPieces.forEach((startPiece, i) => {
         const square = document.createElement('div')
         square.classList.add('square')
+        square.innerHTML = startPiece
+        square.setAttribute('square-id', i)
+        square.classList.add('beige')
+        gameboard.append(square)
     })
 }
+
+createBoard();
