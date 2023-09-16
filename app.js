f3bfb438ede668a724af3413931ccfeb26c525e3
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-16 10:50:04 +0200

    define startPieces

--- app.js
+++ app.js
@@ -4,5 +4,13 @@ const inforDisplay = document.querySelector('#info-display')
 const width = 8
 
 const startPieces = [
-    
+
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook,
 ]
