6959d0deb06651f1cb0251a552ce33712b88bf89
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-18 21:51:08 +0200

    checkIfValid function case pawn & knight

--- app.js
+++ app.js
@@ -114,9 +114,33 @@ function checkIfValid(target){
     switch(piece){
         case 'pawn' :
             const starterRow = [8, 9, 10, 11, 12, 13, 14, 15]
-            if(starterRow.includes(startId) && startId + width * 2 === targetId){
+            if(starterRow.includes(startId) && startId + width * 2 === targetId ||
+                startId + width === targetId ||
+                startId + width - 1 === targetId && document.querySelector(`[square-id="${startId + width - 1}"]`).firstChild ||
+                startId + width + 1 === targetId && document.querySelector(`[square-id="${startId + width + 1}"]`).firstChild 
+            ){
                 return true;
             }
+            break;
+        case 'knight' : 
+            if(
+                startId + width * 2 + 1 === targetId ||
+                startId + width * 2 - 1 === targetId ||
+                startId + width - 2 === targetId ||
+                startId + width + 2 === targetId ||
+                startId - width * 2 + 1 === targetId ||
+                startId - width * 2 - 1 === targetId ||
+                startId - width - 2 === targetId ||
+                startId - width + 2 === targetId 
+            ){
+                return true
+            }  
+            break;
+        case 'bishop' : 
+            if(
+                startId + width + 1 === targetId ||
+                startId + width * 2 + 2
+            ){}     
     }
 }
 
